5f0ca821894fdc6ead75a7c61567360d19bb1643
Nazareno Contreras <nazareno@animus.com.ar>
2023-10-31 11:16:45 -0300

    initial commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->catchPhrase(),
+            'description' => fake()->text(),
+            'date' => fake()->date(),
+            'completed' => fake()->boolean()
+        ];
+    }
+}
